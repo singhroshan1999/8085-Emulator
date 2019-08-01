51e6d236367190214b3e78bc46dc44852a7d458a
roshan singh <singhroshan1999@gmail.com>
2019-08-01 20:57:16 +0530

    buses

--- src/com/singhroshan1999/Buses.java
+++ src/com/singhroshan1999/Buses.java
@@ -1,7 +1,30 @@
 package com.singhroshan1999;
 
 class Buses {
-    private static short _ADDBUS;
-    private static byte _DATABUS;
-    // TODO : Buses
+    private static byte _ADDBUSH,_ADDBUSL; // 8bit high + 8bit low // address buffer + address/data buffer
+    private static byte __DATABUS;  // __ internal
+    // TODO : internal bus
+    private static boolean ALE,MUX;
+
+    static void ADDBUS(byte high,byte low){
+        Buses._ADDBUSH = high;
+        Buses._ADDBUSL = low;
+    }
+    static void D0_7(byte low){
+        Buses._ADDBUSL = low;
+    }
+
+    static byte A8_15(){
+        return _ADDBUSH;
+    }
+
+    static byte A0_7(){
+        return (ALE)?_ADDBUSL:0b00000000;
+    }
+
+
+
+
+
+
 }
